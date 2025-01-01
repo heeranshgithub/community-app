81b9cd840b5ae104afd38c2df1fc0a5c4d09ba66
heeransh <heeranshconnect@gmail.com>
2025-01-02 01:01:43 +0530

    eod 01/01/25

--- client/App.js
+++ client/App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
-import PersonDetails from './src/components/PersonDetails'
-import FamilyTree from './src/components/FamilyTree'
-import Register from './src/screens/Register'
 import Form from './src/components/Form'
-import Login from './src/screens/Login'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 export default function App() {
+  const data = {
+    name: 'Mohandas',
+    number: '1234567890',
+    password: 'secret',
+  }
+
+  const url = 'http://localhost:5001/api/v1/user/register'
+  useEffect(() => {
+    const registerUser = async () => {
+      try {
+        const res = await axios.post(url, data)
+        console.log('res:', res.data) // Logging only the data property for clarity
+      } catch (error) {
+        console.log('Error:', error.message)
+        if (error.response) {
+          console.log('Error Response Data:', error.response.data)
+        }
+      }
+    }
+
+
+    registerUser()
+    console.log('hello')
+  }, [url, data])
   return (
     <View style={styles.container}>
       <StatusBar style="white" />
       <Form />
-      {/* <PersonDetails /> */}
-      {/* <FamilyTree /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
     </View>
   )
 }
